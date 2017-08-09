d99a744e84e888f4e1b787bb64bf97cd466ee390
gaaasparr <gaaasparr@gmail.com>
2017-08-09 10:29:31 -0300

    Ejercicio 2 resuelto

--- 02_Templates/Calculadora.h
+++ 02_Templates/Calculadora.h
@@ -1,9 +1,37 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
 
+template <class T>
 
 class Calculadora {
-
+private:
+    T A;
+    T B;
+public:
+    void setA(const T &A) {
+        Calculadora::A = A;
+    }
+    const T &getA() const {
+        return A;
+    }
+    void setB(const T &B) {
+        Calculadora::B = B;
+    }
+    const T &getB() const {
+        return B;
+    }
+    T sumar(){
+        return A + B;
+        }
+    T restar(){
+        return A - B;
+        }
+    T dividir() {
+        return A / B;
+        }
+    T multiplicar(){
+        return A * B;
+        }
 };
 
 
--- 02_Templates/main.cpp
+++ 02_Templates/main.cpp
@@ -5,5 +5,34 @@ using namespace std;
 
 int main() {
     cout << "Ejercicio 00/02\n" << endl;
+    Calculadora<int> calInt;
+    Calculadora<char> calChar;
+    Calculadora<float> calFloat;
+
+// calculadora de enteros
+
+    calInt.setA(4);
+    calInt.setB(5);
+
+    cout << calInt.sumar() << endl;
+    cout << calInt.dividir() << endl;
+
+// calculadora de float
+
+    calFloat.setA(4);
+    calFloat.setB(5);
+
+    cout << calFloat.sumar() << endl;
+    cout << calFloat.dividir() << endl;
+
+// calculadora de char
+
+    calChar.setA(4);
+    calChar.setB(5);
+
+    cout << calChar.sumar() << endl;
+    cout << calChar.dividir() << endl;
+
     return 0;
+    
 }
