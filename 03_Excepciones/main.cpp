86a226d70c43e67052ce3b04c719e8bf1adbba3f
gaaasparr <gaaasparr@gmail.com>
2017-08-09 10:44:16 -0300

    Ejercicio 3 resuelto

--- 03_Excepciones/main.cpp
+++ 03_Excepciones/main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main() {
     cout << "Ejercicio 00/03\n" << endl;
+    Calculadora<int> calc;
+    calc.setA(300);
+    calc.setB(0);
+
+    try{
+        cout << calc.dividir()<<endl;
+    }
+    catch(int e){
+        cout <<  "B no puede ser 0; " << e << endl;
+    }
+
     return 0;
+
 }
